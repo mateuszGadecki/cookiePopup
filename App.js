94814ec5afdc16869683d80c281e1afe83305a81
Mateusz Gadecki <mateusz.gadecki98@gmail.com>
2021-04-18 16:41:44 +0200

    Calling creators based on the existence of a cookie, fetch partners

--- App.js
+++ App.js
@@ -1,1 +1,47 @@
+import NewPartner from "./modules/PartnersData.js";
+import NewPopup from "./modules/CookiePopup.js";
+import { getCookie } from "./modules/CookieHandlers.js";
 
+let partners, arrOfPartners;
+
+const body = document.querySelector("body");
+const section = document.querySelector("section");
+const cookie = getCookie("idOfAcceptedPartners");
+
+window.addEventListener("load", () => {
+  if (!cookie) {
+    showPopup();
+    importPartners();
+  }
+});
+
+const showPopup = () => {
+  const popup = new NewPopup(
+    "GDPR consent",
+    "Our Partners",
+    "Privacy Policy",
+    "I allow",
+    "Accept",
+    "Reject"
+  );
+  body.appendChild(popup.createPopup());
+  section.classList.add("stop-scrolling");
+};
+
+const importPartners = () => {
+  fetch("https://optad360.mgr.consensu.org/cmp/v2/vendor-list.json")
+    .then((res) => res.json())
+    .then((res) => {
+      partners = res.vendors;
+      arrOfPartners = Object.keys(partners).map((key) => partners[key]);
+      arrOfPartners.forEach((el) => {
+        generatePartners(el.name, el.policyUrl, el.id);
+      });
+    });
+};
+
+const generatePartners = (name, url, id) => {
+  const table = document.querySelector(".table");
+  const newPartner = new NewPartner(name, url, id);
+  table.appendChild(newPartner.createPartnerRecord());
+};
