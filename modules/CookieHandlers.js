bd5b35404c974461a6822e227f8bc52a112fc203
Mateusz Gadecki <mateusz.gadecki98@gmail.com>
2021-04-18 16:36:39 +0200

    Adding functions that save cookies for 24 hours and download previously created cookies

---
+++ modules/CookieHandlers.js
@@ -0,0 +1,20 @@
+export const setCookie = (name, value, days) => {
+  let expires = "";
+  if (days) {
+    let date = new Date();
+    date.setDate(date.getDate() + days);
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+};
+
+export const getCookie = (name) => {
+  const nameEQ = name + "=";
+  const ca = document.cookie.split(";");
+  for (let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == " ") c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+};
